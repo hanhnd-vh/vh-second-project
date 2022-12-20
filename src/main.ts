907f5127ef45b7bcb56bb2eca1977e526f572162
hanhnd <hanhnd156@gmail.com>
2022-12-20 10:53:55 +0700

    initialize project

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 import { ConfigKey } from './common/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get<string>(ConfigKey.PORT));
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    await app.listen(configService.get<string>(ConfigKey.PORT));
 }
 bootstrap();
